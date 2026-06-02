bd3484edccd57c195dc12b1e1f465c6e70aa6595
Pedro González Marcos <pedrogonzalezmarcos02@gmail.com>
2026-06-02 09:49:57 +0200

    feat: add env variable to control Gemini model

--- .github/actions/sphere-changelog/summarize.py
+++ .github/actions/sphere-changelog/summarize.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import json
+import os
 import sys
 from typing import Annotated, List, Optional
 from uuid import UUID, uuid4
@@ -26,7 +27,7 @@ class Scope(str, Enum):
     ui = 'ui'
 
 class Commit(BaseModel):
-    id: str = Field(description="SHA256 ofa a commit.")
+    id: str = Field(description="SHA256 of a commit.")
     message: str = Field(description="Description of the commit message according to the Conventional Commits spec.")
     group: Group = Field(description="Type of the commit.")
     scope: Annotated[Scope, Field(description="Scope of the commit according to the Conventional Commits spec.")] = None
@@ -43,17 +44,19 @@ class Change(BaseModel):
     """
     Expected structured output of the LLM
     """
-    id: UUID = Field(description="Computed uuid4 of a summarized change by LLM.")
+    id: str = Field(description="SHA256 of a commit or UUID v4 (i.e. 3feebd0e-7710-4ee3-95d0-15cd214661fd) of a summarized change by LLM.", max_length=40)
     message: str = Field(description="Description of the change summarized by LLM.")
     group: Group = Field(description="Type of the commit.")
     scope: Annotated[Scope, Field(description="Scope of the summarized commit.")] = None
 
 adapter = TypeAdapter(List[Change])
 
+SYSTEM_INSTRUCTION=("You are a technical writer creating a public changelog. "
+    "Your task is to summarize a list of technical changes into clear, concise, and user-friendly updates.")
 
 def get_prompt(json_input: str):
 	return f"""
-	Objective: Given a JSON input summarize and/or merge commits messages in the following jq path `.commits` and
+	Task: Given a JSON input summarize and/or merge commits messages in the following jq path `.commits` and
 	clasify them as a `feat` or `fix`. Your goal is to generate as few entries as possible in order to get an overview of
 	the features and fixes that were made during one software release to another.
 
@@ -90,9 +93,10 @@ if __name__ == "__main__":
 	input_data = sys.stdin.read()
 	client = genai.Client()
 	response = client.models.generate_content(
-    		model="gemini-2.5-flash-lite",
+                model=os.getenv("GEMINI_MODEL_ID"),
     		contents=get_prompt(input_data),
                 config=types.GenerateContentConfig(
+                    system_instruction=SYSTEM_INSTRUCTION,
                     response_mime_type="application/json",
                     response_json_schema=adapter.json_schema(),
                 ),
